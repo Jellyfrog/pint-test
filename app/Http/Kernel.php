68c585eddf74fd77f00896663588acc194bd624b
Jellyfrog <Jellyfrog@users.noreply.github.com>
2023-05-03 18:57:01 +0200

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -4,8 +4,7 @@ namespace App\Http;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
-class Kernel extends HttpKernel
-{
+class Kernel extends HttpKernel{
     /**
      * The application's global HTTP middleware stack.
      *
@@ -21,7 +20,7 @@ class Kernel extends HttpKernel
      \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
      \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
      \App\Http\Middleware\TrimStrings::class,
-     \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+     \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class
  ];
 
     /**
@@ -36,7 +35,7 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class
         ],
 
         'api' => [
