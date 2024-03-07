36ab25727a1828602a4c288fbf68670b4b595439
Jellyfrog <Jellyfrog@users.noreply.github.com>
2024-03-07 01:18:25 +0100

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -4,8 +4,7 @@ namespace App\Http;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
-class Kernel extends HttpKernel
-{
+class Kernel extends HttpKernel{
     /**
      * The application's global HTTP middleware stack.
      *
@@ -13,8 +12,7 @@ class Kernel extends HttpKernel
      *
      * @var array<int, class-string|string>
      */
-    protected $middleware =
-    [
+    protected $middleware=[
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
         \Illuminate\Http\Middleware\HandleCors::class,
